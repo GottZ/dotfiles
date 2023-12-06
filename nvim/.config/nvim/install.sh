4e0556a023cd763832eeba237e7ad75cbfdcbcbc
Jan-Stefan Janetzky <git@gottz.de>
2023-12-06 10:04:26 +0100

    add dependencies to readme

--- nvim/.config/nvim/install.sh
+++ nvim/.config/nvim/install.sh
@@ -4,5 +4,6 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvi
 nvim -c "source lua/gottz/packer.lua|PackerSync"
 nvim -c "source lua/gottz/packer.lua|PackerUpdate"
 nvim -c "source lua/gottz/packer.lua|Mason"
+nvim -c "source lua/gottz/packer.lua|TSInstall"
 nvim -c "source lua/gottz/packer.lua|TSUpdate"
 nvim -c "source lua/gottz/packer.lua|checkhealth"
