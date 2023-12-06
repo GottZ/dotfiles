548bd83d1877671b171276467e9e2285e0ae35b1
Jan-Stefan Janetzky <git@gottz.de>
2023-12-06 09:58:53 +0100

    added auto install for deps and cleanups

--- nvim/.config/nvim/install.sh
+++ nvim/.config/nvim/install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
+nvim -c "source lua/gottz/packer.lua|PackerSync"
+nvim -c "source lua/gottz/packer.lua|PackerUpdate"
+nvim -c "source lua/gottz/packer.lua|Mason"
+nvim -c "source lua/gottz/packer.lua|TSUpdate"
+nvim -c "source lua/gottz/packer.lua|checkhealth"
