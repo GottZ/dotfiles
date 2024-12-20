7f1fe1b67ac2fa7c25136c6e3269c5be060dcc43
Jan-Stefan Janetzky <git@gottz.de>
2024-12-20 16:30:32 +0100

    add initial dotfile script and add it to path

--- etc/profile.d/00-local-bin.sh
+++ etc/profile.d/00-local-bin.sh
@@ -6,6 +6,10 @@ if [[ -r "$HOME/go/bin" ]]; then
 	append_path "$HOME/go/bin"
 fi
 
+if [[ -r "/opt/dotfiles/.bin" ]]; then
+	append_path "/opt/dotfiles/.bin"
+fi
+
 if [[ -r "$HOME/.cargo/env" ]]; then
 	. "$HOME/.cargo/env"
 fi
