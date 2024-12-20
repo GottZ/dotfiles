744b4d92e37c6e239fc707c942d58202f22a1e12
Jan-Stefan Janetzky <git@gottz.de>
2024-12-20 15:11:55 +0100

    add various other local binary path extensions

--- etc/profile.d/00-local-bin.sh
+++ etc/profile.d/00-local-bin.sh
@@ -1,2 +1,14 @@
-append_path "$HOME/.local/bin"
+if [[ -r "$HOME/.local/bin" ]]; then
+	append_path "$HOME/.local/bin"
+fi
+
+if [[ -r "$HOME/go/bin" ]]; then
+	append_path "$HOME/go/bin"
+fi
+
+if [[ -r "$HOME/.cargo/env" ]]; then
+	. "$HOME/.cargo/env"
+fi
+
 export PATH
+
