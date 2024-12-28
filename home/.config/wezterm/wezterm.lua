9d7c47e155c1df4d5f23cb85136441506bf6c56f
Jan-Stefan Janetzky <git@gottz.de>
2024-12-28 17:47:54 +0100

    initial ghostty config + wezterm theme change

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -4,7 +4,9 @@ local act = wezterm.action
 local config = wezterm.config_builder()
 
 config.font = wezterm.font('Pragmasevka Nerd Font')
-config.color_scheme = 'rose-pine'
+--config.color_scheme = 'rose-pine'
+config.color_scheme = 'zenwritten_dark'
+--config.color_scheme = 'ayu'
 config.ssh_backend = "Ssh2"
 
 config.keys = {
