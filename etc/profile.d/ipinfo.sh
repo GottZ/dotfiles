c9f780e156941b341bc89d0607bf495fa6760d91
Jan-Stefan Janetzky <git@gottz.de>
2025-08-23 11:12:50 +0200

    ipinfo function to easily lookup ip information

---
+++ etc/profile.d/ipinfo.sh
@@ -0,0 +1,12 @@
+ipinfo() {
+  if [[ -f "~/.secrets/ipinfo" ]]; then
+    echo "you need to add your access token to ~/.secrets/ipinfo"
+    return
+  fi
+  local ip="$1"
+  if [[ -z "$ip" ]]; then
+    echo "you need to specify an ip"
+    return
+  fi
+  curl -s "https://api.ipinfo.io/lite/${ip}?token=$(cat ~/.secrets/ipinfo)" | jq .
+}
